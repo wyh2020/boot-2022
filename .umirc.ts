3b55e16da22f9bf5ef9c9eedd419918fc47a3332
王勇华 <wangyonghua@shein.com>
2022-09-01 14:19:27 +0800

    update logo & config

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'dumi';
 const repo = 'boot-2022'; // 项目名
 
 export default defineConfig({
-  title: 'Site Name',
+  title: 'wyh-boot',
   mode: 'site',
+  favicon: '/images/logo.png',
+  logo: '/images/logo.png',
   // more config: https://d.umijs.org/config
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
