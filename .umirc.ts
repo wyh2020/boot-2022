2d4d68cc92cbbcd966ceb65e3596e270612502f5
王勇华 <wangyonghua@shein.com>
2022-09-01 11:11:29 +0800

    add ci.yml

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'boot-2022'; // 项目名
+
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
   // more config: https://d.umijs.org/config
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
